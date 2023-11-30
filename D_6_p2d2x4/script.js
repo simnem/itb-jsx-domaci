6eb530868c706adcc83189ae10c688813f7b6099
simnem <simnem3@gmail.com>
2023-12-01 01:45:49 +0200

    Inicijalni komit za domaci 06

---
+++ D_6_p2d2x4/script.js
@@ -0,0 +1,27 @@
+//  ZADATAK 1
+let ukupanNovac = 2000;
+let autobuskaKarta = Math.floor(ukupanNovac / 5); 
+let smestaj = Math.floor(ukupanNovac / 3);
+let preostaliNovac = ukupanNovac - autobuskaKarta - smestaj;
+
+console.log("Putniku je preostalo " + preostaliNovac + " dinara.");
+
+// ZADATAK 2
+let brojPoglavlja = 12;
+let prviDan = 4;
+let drugiDan = prviDan + 2;
+preostalaPoglavlja = brojPoglavlja - prviDan - drugiDan;
+
+console.log("Čitaocu je preostalo da pročita još " + preostalaPoglavlja + " poglavlja.");
+
+// ZADATAK 3 
+
+let dinPera = 1240;
+let dinMika = 1955;
+let kusur = 900; 
+
+let cenaDzempera = (dinPera + dinMika - kusur) / 2;
+let kusurMika = Math.floor(dinMika - cenaDzempera);
+let kusurPera =  Math.floor(dinPera- cenaDzempera);
+
+console.log("Peri pripada " + kusurPera + " dinara od ukupnog kusura, a Miki pripada " + kusurMika + " od ukupnog kusura.");
