c0c2083fc504b8c76ca158fd44258a7d2c430ed9
simnem <simnem3@gmail.com>
2023-12-06 02:12:14 +0100

    Inicijalni komit 7. domaceg

---
+++ D_7_p2d2x4/script.js
@@ -0,0 +1,26 @@
+// Zadatak 1
+let n = 27
+let v = 80;
+
+let propisi = v / n;
+
+let visakLjudi = n - (v / 3);
+visakLjudi = Math.ceil(visakLjudi)
+
+if (propisi >= 3) {
+    document.write(`<p style="color:green">DA</p>`);
+}else {
+    document.write(`<p style="color:red">NE.Potrebno je ${visakLjudi} osobe izadju</p>`);
+}
+
+// Zadatak 2
+let ukupanBrojStanovnika =  6664449;
+let ukupnoTestiranoStanovnistvo = 19091;
+let ukupanBrojPozitivnih = 5805;
+
+let procenatOdTestiranog = (ukupanBrojPozitivnih / ukupnoTestiranoStanovnistvo) * 100;
+let procenatOdUkupnog = (ukupanBrojPozitivnih / ukupanBrojStanovnika) * 100;
+
+if ( procenatOdTestiranog > 30 || procenatOdUkupnog > 10) {
+    document.write(`<p style="color:red">VANREDNO STANJE!</p>`);
+}
