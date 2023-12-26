be06131c177e9bb71ca2cf0a6cd0dcfc3980d23a
simnem <simnem3@gmail.com>
2023-12-26 15:00:32 +0100

    Dopunjavanje komentara

--- D_13_p2d2x4/script.js
+++ D_13_p2d2x4/script.js
@@ -28,22 +28,22 @@ let knjige = [knjiga1, knjiga2, knjiga3, knjiga4];                       // dekl
 
 let brojac = 0;
 let tabelaHTML = '<table style="margin: 0 auto;">';                                      // deklarisanje variable sa kodom sacuvanim kao string, koji ce kasnije biti dodeljen HTML fajlu
-knjige.forEach( k => {      
-    let procitanoBoja = "grey";
-    let bojaPozadine = "#fefae0";
-    if (k.polje == true){
-        procitanoBoja = "blue";
+knjige.forEach( k => {                                                                   // iteriramo kroz sve objekte koji se nalaze u nizu knjige
+    let procitanoBoja = "grey";                                                          // deklarisanje variable koje ce sluziti kao defaultna vrednosti u koliko uslov nije ispunjen
+    let bojaPozadine = "#fefae0";                                                        // deklarisanje variable koje ce sluziti kao defaultna vrednosti u koliko uslov nije ispunjen
+    if (k.polje == true){                                                                // proveravamo da li variabla u objektu ispunjava uslov
+        procitanoBoja = "blue";                                                          // u koliko ispunjava dodeljujemo joj novu vrednost
      }
-    if (brojac % 2 == 0) { 
-        bojaPozadine ="#606c38";
+    if (brojac % 2 == 0) {                                                               // proveravamo da li variabla u objektu ispunjava uslov
+        bojaPozadine ="#606c38";                                                         // u koliko ispunjava dodeljujemo joj novu vrednost
     }
     brojac ++;
-    tabelaHTML += `<tr style="background-color: ${bojaPozadine}">
+    tabelaHTML += `<tr style="background-color: ${bojaPozadine}">                        
                     <td><img src="${k.urlPutanja}" alt="Slika naslovne strane ${k.naziv}" style="width: 20vh; height: auto;"/></td> 
                     <td><p style="color: ${procitanoBoja}; font-size: 35px; font-family:Sans-Serif; text-align: center;">"${k.naziv}", roman autora ${k.autor}</p"></td>
                   </tr>`;
-    }
+    } // variabli tabelaHTML dodajemo redove i celije u skladu sa zahtevima zadatka
 )
-tabelaHTML += "</table>";
+tabelaHTML += "</table>";   // dodajemo closing tags tabeli
 
-document.body.innerHTML += tabelaHTML;
+document.body.innerHTML += tabelaHTML; // ispisujemo tabelu unutar body tag uz pomoc variable 
